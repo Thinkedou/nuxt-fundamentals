1f55e587237839877095e073360b3b213353a6a5
Thinkedou <r.enguehard@gmail.com>
2024-03-24 22:36:38 +0100

    route params

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+  <nav>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/movies">Movies</NuxtLink>
+  </nav>
     <NuxtPage />
   </div>
 </template>
+<style scoped>
+nav{
+  display:flex;
+  gap:10px;
+}
+</style>
